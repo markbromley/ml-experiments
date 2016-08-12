310ddf04860ba6eba1bf9d5c41b72c42edb98487
Mark Bromley <mark_bromley@ymail.com>
2016-08-12 19:49:42 +0100

    Added class

--- perceptron-1.py
+++ perceptron-1.py
@@ -11,3 +11,10 @@ import numpy
 
 import theano
 import theano.tensor as T
+
+
+from logistic_sgd import LogisticRegression, load_data
+
+class HiddenLayer(object):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None,
+                 activation=T.tanh):
