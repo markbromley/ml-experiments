0d16328e888bab9432097d3bcfb7389e2e7c3796
Mark Bromley <mark_bromley@ymail.com>
2016-08-14 23:10:42 +0100

    Added MLP class.

--- perceptron-1.py
+++ perceptron-1.py
@@ -18,3 +18,15 @@ from logistic_sgd import LogisticRegression, load_data
 class HiddenLayer(object):
     def __init__(self, rng, input, n_in, n_out, W=None, b=None,
                  activation=T.tanh):
+        pass
+
+class MLP(object):
+    def __init__(self, rng, input, n_in, n_hidden, n_out):
+        pass
+
+def test_mlp(learning_rate=0.01, L1_reg=0.00, L2_reg=0.0001, n_epochs=1000,
+             dataset='mnist.pkl.gz', batch_size=20, n_hidden=500):
+    pass
+
+if __name__ == '__main__':
+    test_mlp()
