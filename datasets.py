a551aff19520c577092361db68ea718d1447fd03
Mark Bromley <mark_bromley@ymail.com>
2016-11-02 22:15:58 +0000

    Uploaded dataset generator and perceptron (non GPU version).

---
+++ datasets.py
@@ -0,0 +1,126 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+class Dataset(object):
+
+    def __init__(self, data):
+        # TODO: Add asserts
+        self._data = data
+
+    @staticmethod
+    def create_uniform_distribution(x_scale = 1, 
+                                    y_scale = 1, 
+                                    x_shift = 1, 
+                                    y_shift = 1,
+                                    label = None, 
+                                    data_size = 500):
+        data = np.random.rand(data_size, 3)
+        data[:,0] *= x_scale
+        data[:,1] *= y_scale
+        data[:,0] += x_shift
+        data[:,1] += y_shift
+        # Add the label
+        data[:,2] = label
+        data = Dataset(data)
+        return data
+
+    @staticmethod
+    def create_multivariate_normal_distribution(mean = (0,0), 
+                                                covariance_matrix = ((0,0),(0,0)), 
+                                                data_size = 500,
+                                                label = None):
+        data_points = np.random.multivariate_normal(mean, covariance_matrix, (data_size))
+        data = np.zeros((data_size, 3))
+        data[:,:2] = data_points
+        data[:,2] = label
+        return Dataset(data)
+
+
+    def rotate(self, theta_degrees = 75):
+        return_mat = np.zeros_like(self._data)
+        rot_mat = self._get_rotation_matrix(theta_degrees)
+        return_mat[:,:2] = np.dot(self._data[:,:2], rot_mat.transpose())
+        # Add the labels back on after the rotation
+        return_mat[:,2] = self._data[:,2]
+        return Dataset(return_mat)
+
+    def _get_rotation_matrix(self, theta_degrees):
+        theta = np.radians(theta_degrees)
+        cos, sin = np.cos(theta), np.sin(theta)
+        rotation_matrix = np.matrix([[cos, -sin], [sin, cos]])
+        return rotation_matrix
+
+    @property
+    def x1(self):
+        return self._data[:,0]
+
+    @property
+    def x2(self):
+        return self._data[:,1]
+
+    @property
+    def target(self):
+        return self._data[:,2]
+
+    @property
+    def raw_data(self):
+        return self._data
+
+def generate_dataset(show_plot = False):
+    # Create the uniform distributions
+    class_one = Dataset.create_uniform_distribution(x_scale = 3, 
+                                                    y_scale = 3, 
+                                                    x_shift = 2, 
+                                                    y_shift = 1,
+                                                    label = 1).rotate(75)
+
+    class_two = Dataset.create_uniform_distribution(x_scale = 2, 
+                                                    y_scale = 4, 
+                                                    x_shift = 1, 
+                                                    y_shift = -5,
+                                                    label = 2).rotate(75)
+
+    # Create the multivariate gaussian distributions
+    normal_mean_1 = np.array((-2, -3))
+    normal_cov_1 = np.array(((0.5, 0),(0, 3)))
+    class_three = Dataset.create_multivariate_normal_distribution(mean = normal_mean_1, 
+                                                                  covariance_matrix = normal_cov_1,
+                                                                  label = 3)
+    
+    normal_mean_2 = np.array((-4, -1))
+    normal_cov_2 = np.array(((3, 0.5),(0.5, 0.5)))
+    class_four = Dataset.create_multivariate_normal_distribution(mean = normal_mean_2, 
+                                                                 covariance_matrix = normal_cov_2,
+                                                                 label = 4)
+    # Concatenate and shuffle the four classes
+    all_data = np.concatenate((class_one.raw_data,
+                   class_two.raw_data,
+                   class_three.raw_data,
+                   class_four.raw_data),
+                  axis=0)
+    np.random.shuffle(all_data)
+
+    # Split the data into training, validation and test sets
+    # Assumes number of samples multiple of 4
+    # TODO: Add asserts
+    train_ratio, validation_ratio, test_ratio = 0.5, 0.25, 0.25
+    train_split = all_data.shape[0] * train_ratio
+    validation_split = all_data.shape[0] * validation_ratio
+    test_split = all_data.shape[0] * test_ratio
+
+    training_set = all_data[:train_split, :]
+    validation_set = all_data[:validation_split, :]
+    test_set = all_data[:test_split, :]
+
+    if(show_plot):
+        plt.scatter(class_one.x1, class_one.x2, color='red')
+        plt.scatter(class_two.x1, class_two.x2, color='blue')
+        plt.scatter(class_three.x1, class_three.x2, color = 'green')
+        plt.scatter(class_four.x1, class_four.x2, color = 'cyan')
+        plt.show()
+    return training_set, validation_set, test_set
+
+if __name__ == "__main__":
+    generate_dataset(show_plot = True)
+
+
---
+++ perceptron.py
@@ -0,0 +1,8 @@
+from datasets import generate_dataset
+
+# PERCEPTRON
+def train_perceptron(self):
+    pass
+
+if __name__ == "__main__":
+	generate_dataset(True)
