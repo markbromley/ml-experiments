1ef0e558ff632e7527c166e9431cde88bc68ca84
Mark Bromley <mark_bromley@ymail.com>
2016-11-03 03:05:06 +0000

    Added boundary visualisation.
    Also added learning rate visualisation.

--- perceptron.py
+++ perceptron.py
@@ -1,40 +1,132 @@
 import numpy as np
-from datasets import generate_dataset
+import matplotlib.pyplot as plt
 
-# PERCEPTRON
+from datasets import generate_perceptron_dataset, Dataset
+
+# Perceptron functions
 def train_perceptron(w, x, eta):
+    """ Adjusts the weight matrix for an individual data point. """
+    # Add bias and normalise
+    data_point, target = parse_data_point(x)
 
-    data_point = np.zeros((3,1))
-    print data_point.shape
-    data_point[:2,0] = x[:2]
-    data_point[2,0] = 1
-    target = x[2] - 1
-    threshold_func = lambda x: 0 if x < 0 else 1
+    # Train the perceptron for this data point
     summed_weights = np.dot(w, data_point)
-    error = target - threshold_func(summed_weights)
+    error = target - threshold_function(summed_weights)
     w += eta * error * x
     return w
 
 def evaluate_perceptron(w, D):
+    """ 
+    Returns the number of errors encountered, using the given weight matrix
+    for a given data set. 
+    """
     error_sum = 0
     for data_point in D:
-        if data_point[2] == 1 or data_point[2] == 2:
-            
-            summed_weights = np.dot(w, data_point)
-            error = target - threshold_func(summed_weights)
-if __name__ == "__main__":
-    train, valid, test = generate_dataset(False)
+        # Add bias and normalise
+        data_point, target = parse_data_point(data_point)
+
+        summed_weights = np.dot(w, data_point)
+        error = target - threshold_function(summed_weights)
+        if(error != 0):
+            error_sum += 1
+    return error_sum
+
+def parse_data_point(x):
+    """ 
+    Normalises the data, so that classes lie between 0 and 1. Adds an
+    additional input value - the bias, always 1.
+    """
+   # Add the bias
+    data_point = np.zeros((3,1))
+    data_point[:2,0] = x[:2]
+    data_point[2,0] = 1
+
+    # Normalise the target values (classes in this case are 1,2 so easy - subtract 1)
+    target = x[2] - 1
+    return data_point, target
+
+def threshold_function(x):
+    return 0 if x < 0 else 1
+
+def show_decision_boundary_plot(w):
+    """
+    Shows the decision boundary orthogonal to the weight vector. For visualisation,
+    the original data points are recalculated and displayed, with their labels
+    represented by colour.
+    """
+    # Recreate the original datasets for visualisation
+    class_one = Dataset.create_uniform_distribution(x_scale = 3, 
+                                                    y_scale = 3, 
+                                                    x_shift = 2, 
+                                                    y_shift = 1,
+                                                    label = 1).rotate(75)
+
+    class_two = Dataset.create_uniform_distribution(x_scale = 2, 
+                                                    y_scale = 4, 
+                                                    x_shift = 1, 
+                                                    y_shift = -5,
+                                                    label = 2).rotate(75)
+    plt.scatter(class_one.x1, class_one.x2, color='red')
+    plt.scatter(class_two.x1, class_two.x2, color='blue')
+
+    # Calculate the equation for the decision boundary
+    x1 = -(w[2]/w[0])
+    x2 = 0
+    y1 = 0
+    y2 = -(w[2]/w[1])
+    grad = (y1 - y2) / (x1 - x2)
+
+    # Plot the decision boundary
+    y_list = []
+    x_list = [x - 4 for x in xrange(11)]
+    for x in x_list:
+        y_list.append(grad * x + y1)
+    plt.plot(x_list, y_list,linestyle='-')
+
+    plt.show()
+
+def train_and_evaluate(eta = 0.6, epochs = 200):
+    """
+    # Learning rate
+    # Number of epochs
+    """
+    # Initialise the weights
+    # Train the perceptron on the data set
+    # Evaluate the learned values on the test set
+
+    # Get the data
+    train_set, valid_set, test_set = generate_perceptron_dataset()
 
 
 
-    # initalise weights
+    # Initalise weights
     w = np.random.uniform(low = -0.5, high = 0.5, size = 3)
-    x = np.array([3.16965834,1.64847082,2.])
-    print x[2]
 
-    eta = 0.2
-    for data_point in train:
-        if data_point[2] == 1 or data_point[2] == 2:
-            w = train_perceptron(w, data_point, eta)
-            print w
+    # Keep training until the errors in the test set are below the threshold
+    acceptable_error_threshold = 5
 
+    error_plot_interval = 1
+    error_values = []
+    # Check against the test set for errors, keep training until error
+    # number below threshold
+    while evaluate_perceptron(w, test_set) > acceptable_error_threshold:
+        # Train the perceptron for all train data
+        for n in xrange(epochs):
+            i = 0
+            for data_point in train_set:
+                w = train_perceptron(w, data_point, eta)
+                if i % error_plot_interval == 0 and n == 0:
+                    # Append the percentage error
+                    error_values.append(float(evaluate_perceptron(w, test_set)) / float(test_set.shape[0]))
+    samples = [x*error_plot_interval for x in range(len(error_values)) ]
+    return samples, error_values, w
+
+if __name__ == "__main__":
+    samples, error_values, w = train_and_evaluate()
+
+    print error_values
+    plt.scatter(samples, error_values, label="ETA")
+    plt.legend()
+    plt.show()
+    if(True):
+        show_decision_boundary_plot(w)
